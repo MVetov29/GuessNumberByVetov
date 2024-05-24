5436091dec46b6b0136a4b1015b276f19b75ca1e
unknown <martin.vetov@abv.bg>
2024-05-24 14:30:10 +0300

    Ver. 1.0.1

--- guess_a_number_bitch.py
+++ guess_a_number_bitch.py
@@ -1,12 +1,21 @@
 import random
 computer_number = random.randint(1, 100)
 
+counter = 0
+
 while True:
-    number = int(input("Vuvedi nomer:"))
+    number = int(input("Enter number bitch: "))
     if computer_number > number:
+        counter += 1
         print("The number you entered is lower than the desire number")
     elif computer_number < number:
+        counter += 1
         print("The number you entered is greater than the desire number")
-    else:
-        print("Congratulation, you guess the number! You win Bitch!!!")
-        exit()
+
+    if counter == 3:
+        if computer_number == number:
+            print("Congratulation, you guess the number! You win Bitch!!!")
+            exit()
+        else:
+            print("Try again.... Ha-Ha-Ha!!!")
+            exit()
